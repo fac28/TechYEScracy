65c386055b12658ee0a3c1bfc5527b469c6d657b
JamesESS <60142087+JamesESS@users.noreply.github.com>
2023-10-10 14:15:07 +0100

    start home template

--- src/templates/home.js
+++ src/templates/home.js
@@ -1,16 +1,24 @@
 const { layout } = require('./layout.js');
 
-function home() {
+function home(LOGIN_URL,user) {
     const title = 'TechYEScracy';
+    let logIn = `<a href="${LOGIN_URL}">Log in with GitHub</a>`;
+    if (user) {
+        logIn = `
+        <h2><img src=${user.avatar_url}>${user.login} Followers: ${user.followers}</h2>
+        <form action="/log-out" method="post"><button>Log out</button></form>`;
+    }
     const content = /*html*/ ` 
     <div class="banner">
         <div class="title">
             <h1>TechYEScracy</h1>
         </div>
-    
+        <div class="">
+            ${logIn}
+        </div>
     </div>
     `;
 
     return layout({ title, content });
 }
-module.exports = { home, users };
+module.exports = { home };
