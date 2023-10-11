f3710fadc9ab0609b30029503dbcb61a82fc7561
sgroil <laurencesgroi@hotmail.co.uk>
2023-10-11 17:46:07 +0100

    vote once

--- src/routes/vote.js
+++ src/routes/vote.js
@@ -9,17 +9,15 @@ router.post('/', (req, res) => {
   try {
     const user = req.signedCookies ? req.signedCookies.user : false;
     const { poll_id, vote_type } = req.query;
-    const userID = getUserByUsername(user.login)
-  
+    const userID = getUserByUsername(user.login);
+
     console.log(userID);
- 
 
     if (user) {
+      // Create a new vote record in the "votes" table
+      createVote(userID.id, poll_id, vote_type);
       // Update the poll
       updatePoll(poll_id, vote_type, parseInt(user.followers));
-
-      // Create a new vote record in the "votes" table
-      createVote(userID.id, poll_id, vote_type); // Assuming you have a user.id available in your session
     }
 
     return res.redirect('/');
