66121b9e7c9bc7ffceb0e325cfc4c30950b1bcd8
ShaughnAnderson94 <andersji94@gmail.com>
2023-10-11 15:02:25 +0100

    try catch added to routes

--- src/routes/log-out.js
+++ src/routes/log-out.js
@@ -1,13 +1,18 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-const { removeSession } = require("../models/sessions.js");
+const { removeSession } = require('../models/sessions.js');
 
-router.post("/log-out", (req, res) => {
-  const sid = req.signedCookies.sid;
-  removeSession(sid);
-  res.clearCookie("user");
-  res.clearCookie("sid");
-  res.redirect("../");
+router.post('/log-out', (req, res) => {
+  try {
+    const sid = req.signedCookies.sid;
+    removeSession(sid);
+    res.clearCookie('user');
+    res.clearCookie('sid');
+    res.redirect('../');
+  } catch {
+    console.error('Error with route:', error.message);
+    throw error;
+  }
 });
 
 module.exports = router;
