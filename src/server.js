7846ca7160052423d94fa5366a21267722378673
sgroil <laurencesgroi@hotmail.co.uk>
2023-10-11 10:25:48 +0100

    added middleware

--- src/server.js
+++ src/server.js
@@ -6,36 +6,35 @@ const homeRout = require("./routes/home");
 const formRout = require("./routes/form");
 const authenticate = require("./routes/authenticate");
 const logOut = require("./routes/log-out");
-// const { getSession, removeSession } = require("./models/sessions.js");
+const { getSession, removeSession } = require("./models/sessions.js");
 const body = express.urlencoded({ extended: false });
-// const cookies = cookieParser(process.env.COOKIE_SECRET);
+
 
 //const templates = require('./templates')
 
-// function sessions(req, res, next) {
-//   const sid = req.signedCookies.sid;
-//   const session = getSession(sid);
-//   if (session) {
-//     const expiry = new Date(session.expires_at);
-//     const today = new Date();
-//     if (expiry < today) {
-//       removeSession(session.id);
-//       res.clearCookie(sid);
-//     }
-//     req.session = session;
-//   }
-//   next();
-// }
+function sessions(req, res, next) {
+  const sid = req.signedCookies.sid;
+  const session = getSession(sid);
+  if (session) {
+    const expiry = new Date(session.expires_at);
+    const today = new Date();
+    if (expiry < today) {
+      removeSession(session.id);
+      res.clearCookie(sid);
+    }
+    req.session = session;
+  }
+  next();
+}
 
 //Middleware
-// app.use(cookieParser(process.env.COOKIE_SECRET));
+app.use(cookieParser(process.env.COOKIE_SECRET));
 app.use((req, res, next) => {
   const time = new Date().toLocaleTimeString("en-GB");
   console.log(`${time} ${req.method} ${req.url}`);
   next();
 });
-// app.use(sessions);
-// app.use(cookies);
+app.use(sessions);
 app.use(body);
 app.use(express.static("public"));
 
