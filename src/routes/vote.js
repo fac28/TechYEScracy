c4e5ce9c8885b51612ee27f124b972c7704692e2
sgroil <laurencesgroi@hotmail.co.uk>
2023-10-12 11:26:56 +0100

    vote fix

--- src/routes/vote.js
+++ src/routes/vote.js
@@ -5,7 +5,7 @@ const { getUserByUsername } = require('../models/user.js');
 
 const router = express.Router();
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   try {
     const user = req.signedCookies ? req.signedCookies.user : false;
     const { poll_id, vote_type } = req.query;
@@ -18,7 +18,7 @@ router.post('/', (req, res) => {
 
     if (user) {
       // Create a new vote record in the "votes" table
-      createVote(userID.id, poll_id, vote_type);
+      await createVote(userID.id, poll_id, vote_type);
       // Update the poll
       updatePoll(poll_id, vote_type, parseInt(user.followers));
     }
@@ -26,6 +26,7 @@ router.post('/', (req, res) => {
     return res.redirect('/');
   } catch (error) {
     console.error('Error with route:', error.message);
+    res.status(500).redirect('/');
     return;
   }
 });
